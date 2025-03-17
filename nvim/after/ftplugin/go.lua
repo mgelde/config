cdcf617522c90d59bad421bcb070565fdfdc8736
Marcus Gelderie <redmnic@gmail.com>
2025-03-17 17:27:58 +0100

    nvim/go: Automatically enable coq_nvim for Go.

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -6,3 +6,5 @@ vim.opt.softtabstop=0
 
 vim.keymap.set('', ',i', function() vim.lsp.buf.format({range=nil}) end, {noremap=true})
 vim.keymap.set('', ',o', function() vim.lsp.buf.format() end, {noremap=true})
+
+vim.cmd.COQnow('--shut-up')
