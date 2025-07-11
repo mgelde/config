bcdce4373ea8263d55f5dba8bc3f2ea191e8c3db
Marcus Gelderie <redmnic@gmail.com>
2025-07-11 14:59:41 +0200

    nvim/colors: Function to automatically approximate cterm.

--- nvim/colors/breen.lua
+++ nvim/colors/breen.lua
@@ -11,22 +11,48 @@ vim.cmd.syntax('reset')
 vim.g.colors_name = 'breen'
 
 
-local background_base_color = '#07022A'
-
-local function make_highlight_statement(args)
-end
-
 
 local function truecolor2termcolor(colorcode)
     local bit = require('bit')
-    local number = tonumber(colorcode:sub(1, #colorcode))
+    local number = tonumber(colorcode:sub(2, #colorcode), 16)
     local red = bit.band(bit.rshift(number, 16), 0xFF)
     local green = bit.band(bit.rshift(number, 8), 0xFF)
     local blue = bit.band(number, 0xFF)
-    return string.format('%d', bit.lshift((red * 7 / 255), 5) + bit.lshift((green * 7 / 255), 2) + (blue * 3 / 255))
+    return bit.lshift((red * 7 / 255), 5) + bit.lshift((green * 7 / 255), 2) + (blue * 3 / 255)
 end
 
-vim.cmd.highlight('CursorLine    guibg=#0D3138 ctermbg=236')
+
+
+local function highlight(args)
+    local guifg = args.guifg and string.format('guifg=%s', args.guifg) or ''
+    local guibg = args.guibg and string.format('guibg=%s', args.guibg) or ''
+    local gui = args.gui and string.format('gui=%s', args.gui) or ''
+    local ctermfg = args.ctermfg or (args.guifg and string.format('ctermfg=%d', truecolor2termcolor(args.guifg))) or ''
+    local ctermbg = args.ctermbg or (args.guibg and string.format('ctermbg=%d', truecolor2termcolor(args.guibg))) or ''
+    local cterm = args.cterm or (args.gui and string.format('cterm=%s', args.gui)) or ''
+    vim.cmd.highlight(string.format('%s %s %s %s %s %s %s',
+        args.group,
+        guifg,
+        guibg,
+        gui,
+        ctermbg,
+        ctermfg,
+        cterm
+    ))
+end
+
+local background_base_color = '#0f282d'
+local background_complementary_color = '#2d140f'
+local background_dark_color = '#09191c'
+local background_light_color = '#39494c'
+
+highlight{group='Normal', guifg='#e7e7f0', guibg=background_base_color, gui='none'}
+
+highlight{group='CursorLine',    guibg=background_light_color}
+highlight{group='LineNr',        guifg='#808080', guibg=background_dark_color, gui='none' }
+highlight{group='CursorLineNr',  guifg='#B0D0B0',               gui='bold' }
+
+
 vim.cmd.highlight('ColorColumn   guibg=#2d2d2d ctermbg=236')
 vim.cmd.highlight('CursorColumn  guibg=#2d2d2d ctermbg=236')
 vim.cmd.highlight('MatchParen    guifg=#d0fff0  guibg=#0b4338 gui=bold ctermfg=157 ctermbg=237 cterm=bold')
@@ -34,10 +60,7 @@ vim.cmd.highlight('Pmenu		 guifg=#ffffff  guibg=#091F24 ctermfg=255 ctermbg=238'
 vim.cmd.highlight('PmenuSel		 guifg=#0FF29F guibg=#14444F ctermfg=0 ctermbg=148 cterm=reverse')
 
 vim.cmd.highlight('Cursor       guifg=NONE    guibg=#626262 gui=none ctermbg=241')
-vim.cmd.highlight('Normal       guifg=#e7e7f0 guibg=#0A2328 gui=none ctermfg=253 ctermbg=234')
 vim.cmd.highlight('NonText      guifg=#808080 guibg=#0A2328 gui=none ctermfg=244 ctermbg=235')
-vim.cmd.highlight('LineNr       guifg=#808080 guibg=#081820 gui=none ctermfg=244 ctermbg=232')
-vim.cmd.highlight('CursorLineNr guifg=#B0D0B0               gui=bold ctermbg=241')
 vim.cmd.highlight('StatusLine   guifg=#d3d3d5 guibg=#444444 gui=italic ctermfg=253 ctermbg=238 cterm=italic')
 vim.cmd.highlight('StatusLineNC guifg=#939395 guibg=#444444 gui=none ctermfg=246 ctermbg=238')
 vim.cmd.highlight('VertSplit    guifg=#444444 guibg=#444444 gui=none ctermfg=238 ctermbg=238')
